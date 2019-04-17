d62ee14fa49c5c16d22604b3967b9ad542d31688
Brennon Nuckols <brennuck@gmail.com>
2019-04-17 15:09:59 -0600

    Finished but error

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
-// import actions
+import {getCharacter} from "../actions";
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -10,12 +10,12 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
-    // call our action
+    this.props.getCharacter()
   }
 
   render() {
     if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+      <h1>Loading.......................</h1>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +27,15 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps = state => ({
+  fetching: state.fetching,
+  characters: state.characters
+});
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
+    getCharacter
   }
 )(CharacterListView);
